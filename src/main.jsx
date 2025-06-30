dbc1a29005bd9e4b4cf7dd735ef67435375a906a
athulya2307 <150112599+athulya2307@users.noreply.github.com>
2025-06-30 13:40:13 +0530

    final

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
+// filepath: c:\Users\Admin\OneDrive\Desktop\CODES\my-app\src\main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
+import PitchGraphMulti from './App.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <PitchGraphMulti />
   </React.StrictMode>
 );
